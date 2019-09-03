8580fc6820438e1808f4414f8fc039fd55c7e7ee
kunjan343 <kunjanpatelrock@gmail.com>
2019-09-03 12:26:43 -0400

    add class 7 programs

---
+++ PHP_WEB/WEB7/welcome/welcome.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<?php
+$name = $_GET["name"];
+$age = $_GET["age"];
+
+echo "Name is $name";
+echo "Age is $age";
+?>
+</body>
+</html>
